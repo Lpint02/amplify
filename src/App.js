a94a89b70574da6bf4908a956db3bbae5c1f4205
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-07-24 16:16:45 +0200

    fix switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Parenzo from './Parenzo';
 import Cruciani from './Cruciani';
@@ -8,12 +8,12 @@ import Spatalino from './Spatalino';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/parenzo" component={Parenzo} />
-        <Route path="/cruciani" component={Cruciani} />
-        <Route path="/spatalino" component={Spatalino} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/parenzo" element={<Parenzo />} />
+        <Route path="/cruciani" element={<Cruciani />} />
+        <Route path="/spatalino" element={<Spatalino />} />
+      </Routes>
     </Router>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const products = [
   { title: 'Spatalino', isEbreo: false, id: 1, link: '/spatalino' },
   { title: 'Parenzo', isEbreo: true, id: 2, link: '/parenzo' },
@@ -5,37 +7,37 @@ const products = [
 ];
 
 function ProductButton({ link }) {
-  return (
-    <a href={link}>
-      <button>
-        Go to section
-      </button>
-    </a>
-  );
-}
-
-export default function Home() {
-  const listItems = products.map(product => (
-    <li
-      key={product.id}
-      style={{
-        color: product.isEbreo ? 'red' : 'black',
-        marginBottom: '10px',
-        display: 'flex',
-        alignItems: 'center'
-      }}
-    >
-      <span style={{ marginRight: '10px' }}>{product.title}</span>
-      <ProductButton link={product.link} />
-    </li>
-  ));
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Membri della Zanzara</h1>
-        <ul>{listItems}</ul>
-      </header>
-    </div>
-  );
-}
+    return (
+      <Link to={link}>
+        <button>
+          Go to section
+        </button>
+      </Link>
+    );
+  }
+  
+  export default function Home() {
+    const listItems = products.map(product => (
+      <li
+        key={product.id}
+        style={{
+          color: product.isEbreo ? 'red' : 'black',
+          marginBottom: '10px',
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{ marginRight: '10px' }}>{product.title}</span>
+        <ProductButton link={product.link} />
+      </li>
+    ));
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Membri della Zanzara</h1>
+          <ul>{listItems}</ul>
+        </header>
+      </div>
+    );
+  }

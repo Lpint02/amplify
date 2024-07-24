45aca0c2a81b0b9286535eb44fe5731549d03236
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-07-24 11:53:28 +0200

    revert2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Home from './components/Home';
+/*import Home from './components/Home';
 import About from './components/About';
-import Contact from './components/Contact';
+import Contact from './components/Contact';*/
 import './App.css';
 
-
+/*
 function App() {
   return (
     <Router>
@@ -30,4 +30,25 @@ function App() {
     </Router>
   );
 }
-export default App;
+export default App;*/
+function App() {
+	return (
+	  <div className="App">
+		<header className="App-header">
+		  <img src={logo} className="App-logo" alt="logo" />
+		  <p>
+			Edit <code>src/App.js</code> and save to reload.
+		  </p>
+		  <a
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
+		  >
+			Learn React
+		  </a>
+		</header>
+	  </div>
+	);
+  }
+  export default App;

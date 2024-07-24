c40f1de5963376bd33f8011b5975259a18940330
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-07-24 16:46:13 +0200

    css applied

--- src/Parenzo.js
+++ src/Parenzo.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.css';
 export default function Parenzo() {
   return (
-    <div>
-      <h1>Parenzo</h1>
-      <p>This is content for Parenzo.</p>
-    </div>
+    <div className="App">
+    <header className="App-header">
+      <h1>MA IO TI QUERELO</h1>
+      <img 
+          src="https://wips.plug.it/cips/libero.it/magazine/cms/2022/07/david-parenzo.jpg" 
+          alt="David Parenzo" 
+          className="Parenzo-image" 
+        />
+    </header>
+  </div>
   );
 }
--- src/Spatalino.js
+++ src/Spatalino.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './App.css';
 export default function Spatalino() {
   return (
-    <div>
-      <h1>Spatalino</h1>
-      <p>This is content for Spatalino.</p>
-    </div>
+    <div className="App">
+    <header className="App-header">
+      <h1>CIBO NO BUONO</h1>
+      <h1>ITALIANIIIII</h1>
+        <img 
+          src="https://pbs.twimg.com/profile_images/1193448180266029057/6f09jm6x_400x400.jpg" 
+          alt="Enzo Spatalino" 
+          className="Spatalino-image" 
+        />
+    </header>
+  </div>
   );
 }

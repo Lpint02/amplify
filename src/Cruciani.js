d6ae89d452b5840a134b2cc83dd6b54e499023a4
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-08-05 15:47:43 +0200

    ripristinato

---
+++ src/Cruciani.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+export default function Cruciani() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>ITALIANIIIII</h1>
+        <img 
+          src="https://www.arabonormannaunesco.it/wp-content/uploads/2024/06/giuseppe-cruciani-20240621-arabonormannounesco.it_.jpg" 
+          alt="Giuseppe Cruciani" 
+          className="Cruciani-image" 
+        />
+    </header>
+  </div>
+  );
+}
